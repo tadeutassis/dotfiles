abf1a0de953384df95c5f8987115541ebc409431
tadeutassis <tadeutassis@gmail.com>
2024-10-15 20:07:52 -0300

    back to bash

--- .config/nvim/after/plugin/lualine-config.lua
+++ .config/nvim/after/plugin/lualine-config.lua
@@ -9,7 +9,7 @@ require("lualine").setup({
     lualine_a = { "mode" },
     lualine_b = { "branch", "diff", "diagnostics" },
     lualine_c = { "filename" },
-    lualine_x = { "filetype" },
+    lualine_x = { "encoding","filetype" },
     lualine_y = { "progress" },
     lualine_z = { "location" },
   },
